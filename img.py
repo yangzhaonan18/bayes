0725fbcb529d6d25cd62d92d5229176e7be2ef93
yangzhaonan18 <yangzhaonan18@qq.com>
2019-12-03 10:20:21 +0000

    add 3D plot map

--- img.py
+++ img.py
@@ -1,6 +1,40 @@
 import gdal 
-import geos
+# import geos
 
-# geo = gdal.Open('data/USGS_NED_13_n40w112_IMG/USGS_NED_13_n40w112_IMG.img') 
-# arr = geo.ReadAsArray() 
-# print(repr(arr) )
+geo = gdal.Open('data/clip_USGS_NED_13_n40w112_IMG/clip_USGS_NED_13_n40w112_IMG.img') 
+data = geo.ReadAsArray() 
+# print(repr(data) )
+# print(data)
+print(data.shape)
+ 
+h, w = data.shape
+ 
+
+
+from matplotlib import pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+fig = plt.figure()
+ax = Axes3D(fig)
+# X = np.arange(-4, 4, 0.25)
+X = np.arange(0, h, 1)
+Y = np.arange(0, w, 1)
+X, Y = np.meshgrid(X, Y)
+print(X.shape)
+data = data.T
+Z = np.array(data)
+print(Z.shape)
+# R = np.sqrt(X**2 + Y**2)
+
+# Z = np.sin(R)
+
+ 
+ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap='rainbow')
+
+plt.show()
+
+
+
+
+ 
--- just_for_test.py
+++ just_for_test.py
@@ -3,35 +3,52 @@
 # pi = 3.1416
 # print(math.sin(30 * (pi / 180)))
 
-import random
 
-class Transfer_matrix:
-	def __init__(self,):
-		self.name  = "yzn"
-		self.x = 108
-		self.y = 2
-		self.z = self.__cal(self.x, self.y)
+################################################################
+# import random
+
+# class Transfer_matrix:
+# 	def __init__(self,):
+# 		self.name  = "yzn"
+# 		self.x = 108
+# 		self.y = 2
+# 		self.z = self.__cal(self.x, self.y)
 	
-	def add_one(self,):
-		print("666")
+# 	def add_one(self,):
+# 		print("666")
 	
-	def __cal(self,x, y):
-		return x + y  + random.randint(100, 100000)
-	def p(self,):
-		print(self.z)
+# 	def __cal(self,x, y):
+# 		return x + y  + random.randint(100, 100000)
+# 	def p(self,):
+# 		print(self.z)
 	
 		 
 
 
 
-if __name__ == "__main__":
-	tm = Transfer_matrix()
-	x = tm.add_one()
-	tm.p()
-	tm.p()
-	tm.p()
-	tm.p()
-	tm.p()
-	tm.p()
-	tm.p()
-	tm.p()
+# if __name__ == "__main__":
+# 	tm = Transfer_matrix()
+# 	x = tm.add_one()
+# 	tm.p()
+# 	tm.p()
+# 	tm.p()
+# 	tm.p()
+# 	tm.p()
+# 	tm.p()
+# 	tm.p()
+# 	tm.p()
+# #######################################
+
+
+import numpy as np
+
+X = np.arange(0, 5, 1)
+Y = np.arange(0, 2, 1)
+print(X)
+X, Y = np.meshgrid(X, Y)
+print(X)
+print(Y)
+
+
+R = np.sqrt(X**2 + Y**2)
+print(R)
